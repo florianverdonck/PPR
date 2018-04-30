d98c4564315202779d95018f117728b17347e63e
Florian <florianverdonck@gmail.com>
2018-04-30 14:53:16 +0200

    Updated client
    Prompt isn't pretty printing

--- client.c
+++ client.c
@@ -37,20 +37,57 @@
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+
+/*
+
+
+TO-DO :
+
+- liste des virements (externalisée ? STRUCT SET avec méthode d'ajout ?)
+- auto-double size struct
+
+*/
+
 
 #include "client.h"
 
+// ouverture des deux pipes servant à communiquer
+int pipe_transfer_fd[2];
+int pipe_timer_fd[2];
+
+char*	server_address;
+int	 	server_port;
+int  	source_account;
+int		transfer_delay;
+
+// Array containing all reccurent transfers
+Transfer* transfers_list;
+int transfers_list_size = 0;
+int transfers_in_list = 0;
+
 int main(int argc, char* argv[]) {
 
-	char*	server_address = argv[0];
-	int	 	server_port = atoi(argv[1]);
-	char* 	source_account = argv[2];
-	int		transfer_delay = atoi(argv[3]);
+	if (argc != 5) {
+		printf("Le nombre de paramètres est incorrect\n");
+		print_usage();
+		exit(1);
+	}
+	
+	server_address = argv[1];
+	server_port = atoi(argv[2]);
+	source_account = atoi(argv[3]);
+	transfer_delay = atoi(argv[4]);
+
+
+	if (pipe(pipe_transfer_fd) < 0) {
+		printf("Erreur création pipe transfer\n");
+		exit(1);
+	}
+
+	if (pipe(pipe_timer_fd) < 0) {
+		printf("Erreur création pipe timer\n");
+		exit(1);
+	}
 
 	pid_t pid = fork();
 
@@ -70,7 +107,7 @@ int main(int argc, char* argv[]) {
 			prompt();
 		} else {
 			// Child 2
-			timer_child();
+			timer_child(transfer_delay);
 		}
 
 	} else {
@@ -85,6 +122,12 @@ int main(int argc, char* argv[]) {
 void prompt() {
 	printf("*** CONSOLE DE VIREMENTS ***\n");
 
+	// On ferme la lecture sur celui là
+	close(pipe_transfer_fd[0]);
+	// On ferme la lecture et écriture sur l'autre
+	close(pipe_timer_fd[0]);
+	close(pipe_timer_fd[1]);
+
 	while (1) {
 		printf("> ");
 		char command[30];
@@ -93,42 +136,68 @@ void prompt() {
 			perror("Erreur lors du fgets");
 		}
 
-		strip_line_break(&command);
+		strip_line_break(command);
 
 		char params[3][20];
 
-		getCommandParams(command, params);
+		get_command_params(command, params);
+		Message msg = create_message(params[0], params[1], params[2]);
 
-		if (strcmp(params[0], "*")) {
+		if (msg.type == 0) {
 			// Envoi du virement avec les params
-		} else if (strcmp(params[0], "+")) {
-			// Envoi du virement avec les params
+		} else if (msg.type == 1) {
+			write(pipe_transfer_fd[1], &msg, sizeof(msg));
 		} else {
 			printf("L'opération n'est pas reconnue, veuillez réessayer\n");
 		}
 
 	}
+	// On ferme l'écriture sur celui là
+	close(pipe_transfer_fd[1]);
 }
 
-int getCommandParams(char *command, char params[3][20])
+int get_command_params(char *command, char params[3][20])
 {
-	int n=0,i,j=0;
+	int n=0, i, j=0;
 	
-	for(i=0;1==1;i++)
+	for(i=0; 1==1; i++)
 	{
-		if(command[i]!=' '){
-			params[n][j++]=command[i];
+		if(command[i] != ' '){
+			params[n][j++] = command[i];
 		}
 		else{
-			params[n][j++]='\0';
+			params[n][j++] = '\0';
 			n++;
-			j=0;
+			j = 0;
 		}
-		if(command[i]=='\0')
+		if(command[i] == '\0')
 		    break;
 	}
 	return n;
 	
+}
+
+Message create_message(char type[20], char destination[20], char amount[20]) {
+	Message msg;
+
+	if (strcmp(type, "*")) {
+		msg.type = 0;
+	} else if (strcmp(type, "+")) {
+		msg.type = 1;
+	} else {
+		printf("L'opération n'est pas reconnue, veuillez réessayer\n");
+		msg.type = -1;
+		return msg;
+	}
+
+	Transfer transfer;
+	transfer.source = source_account;
+	transfer.destination = atoi(destination);
+	transfer.amount = atoi(amount);
+
+	msg.transfer = transfer;
+
+	return msg;
 }
 
 int strip_line_break(char* command) {
@@ -142,10 +211,104 @@ int strip_line_break(char* command) {
 	}
 }
 
-void timer_child() {
-	printf("*** TIMER CHILD *** \n");
+void timer_child(int transfer_delay) {
+	printf("*** TIMER CHILD (each %i seconds) *** \n", transfer_delay);
+
+	// On ferme tout
+	close(pipe_transfer_fd[0]);
+	close(pipe_transfer_fd[1]);
+
+	// On ferme la lecture sur l'autre
+	close(pipe_timer_fd[0]);
+
+	while(1) {
+		// Every tick on transfer delay, send a message to the other child
+		sleep(transfer_delay);
+		send_tick_signal();
+	}
 }
 
 void reccurent_transfers_child() {
 	printf("*** RECCURENT TRANSFERS CHILD *** \n");
+
+	// On ferme l'écriture sur celui là
+	close(pipe_transfer_fd[1]);
+
+	// On ferme l'écriture sur l'autre
+	close(pipe_timer_fd[1]);
+
+	Message msg;
+
+	while (1==1) {
+
+		int resultRead = read(pipe_transfer_fd[0], &msg, sizeof(msg));
+
+		if (resultRead == 0) {
+			printf("NO MSG\n");
+		} else if (resultRead == -1) {
+			// Erreur lors de la lecture
+			printf("ERRUER L\n");
+		} else {
+			printf("Recu msg sur PIPE\n");
+			// Enregistrement du virement dans le set
+			add_recurrent_transfer(&msg.transfer);
+		}
+
+	}
+
+	// Stores reccurent transfers in an array
+	// When it receives the tick signal, it loops over the array and send all transfers
+
+}
+
+void print_usage() {
+	printf("./client [adr] [port] [num] [delay]\n");
+}
+
+void send_tick_signal() {
+	//
+}
+
+int add_recurrent_transfer(Transfer* toAdd) {
+
+	write(1, "Ajout virement récurrent\n", strlen("Ajout virement récurrent\n"));
+
+	if (transfers_list_size == transfers_in_list) {
+		// Liste non initialisée ou trop courte
+		if (transfers_list_size == 0) {
+			// Si la liste n'a jamais été initialisée
+			transfers_list = (Transfer*) malloc(sizeof(toAdd) * INIT_TRANSFERS_LIST_SIZE);
+			if (transfers_list != NULL) {
+				// Si le malloc a réussi
+				transfers_list_size = INIT_TRANSFERS_LIST_SIZE;
+			} else {
+				perror("Erreur lors du malloc");
+				exit(1);
+			}
+		} else {
+			// Elle a déjà été initialisée mais est maintenant trop courte
+			transfers_list = (Transfer*) realloc(transfers_list, sizeof(toAdd) * (transfers_list_size * 2));
+			if (transfers_list != NULL) {
+				// Si le malloc a réussi
+				transfers_list_size = (transfers_list_size * 2);
+			} else {
+				perror("Erreur lors du malloc");
+				exit(1);
+			}
+		}
+	}
+
+	*(transfers_list + transfers_in_list) = *toAdd;
+	transfers_in_list++;
+
+	print_transfers_list();
+
+	return 1;
+
+}
+
+void print_transfers_list() {
+	printf("Liste des virement \n ----------------- \n");
+	for (Transfer *transfer = transfers_list; transfer - transfers_list < transfers_in_list; transfer++)
+	    printf("Virement (%i -> %i : %i)\n", transfer->source, transfer->destination, transfer->amount);
 }
--- client.h
+++ client.h
@@ -1,9 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "transfer.h"
+#include "message.h"
+
+#define INIT_TRANSFERS_LIST_SIZE 2
+
 void prompt();
 
-void timer_child();
+void timer_child(int transfer_delay);
 
 void reccurent_transfers_child();
 
 int strip_line_break(char* command);
 
-int getCommandParams(char *base, char target[10][20]);
+int get_command_params(char *base, char target[10][20]);
+
+void print_usage();
+
+void send_tick_signal();
+
+Message create_message(char type[20], char destination[20], char amount[20]);
+
+int add_recurrent_transfer(Transfer* toAdd);
+
+void print_transfers_list();
